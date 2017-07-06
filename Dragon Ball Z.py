512e8c6bab654e047fb437c9444b9784a7888913
Fabricio\Jhon <maria.cordova01@epn.edu.ec>
2017-07-06 17:09:42 -0500

    Pantalla de presentacion, (solo interfaz)

--- Dragon Ball Z.py
+++ Dragon Ball Z.py
@@ -13,3 +13,52 @@ while True:
             sys.exit()
     pygame.display.update()
             
+############## Pantalla de presentacion########
+# rojo, verde, azul
+from pygame import *
+
+init()
+v = display.set_mode((700, 400))
+display.set_caption("DRAGON BALL Z")
+salir = False
+imagen = image.load("g2.jpg")
+font = font.SysFont("Showcard Gothic", 15)
+######## texto botones ##########
+txtb1 = font.render("Jugar", True, (0, 0, 0))
+txtb2 = font.render("Instruciones", True, (0, 0, 0))
+txtb3 = font.render("Puntaje", True, (0, 0, 0))
+velo = 5
+posy = 200
+
+
+def botones():
+    draw.rect(v, (100, 90, 255), (80, 200, 100, 30))
+    v.blit(txtb1, (100, 210))
+
+    draw.rect(v, (0, 255, 50), (50, 250, 150, 30))
+    v.blit(txtb2, (70, 257))
+
+    draw.rect(v, (100, 90, 255), (80, 300, 100, 30))
+    v.blit(txtb3, (100, 310))
+
+
+while salir == False:
+    for evento in event.get():
+        if evento.type == QUIT:
+            salir = True
+            quit()
+            raise SystemExit
+        elif evento.type == KEYDOWN:
+            if evento.key == K_UP:
+                posy = velo
+            elif evento.key == K_DOWN:
+                posy = velo
+        elif evento.type == KEYUP:
+            if evento.key == K_UP:
+                print("tecla arriba")
+            elif evento.key == K_DOWN:
+                print("tecla abajo")
+    v.fill((255, 255, 255))
+    v.blit(imagen, (0, 0))
+    botones()
+    display.update()
