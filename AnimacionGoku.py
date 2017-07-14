f2c03abcb00be95ea773b4f2d9c707c1f9b2af54
Ivonne Vega\PERSONAL <ivonne01101@hotmail.com>
2017-07-14 17:34:41 -0500

    fun Sprite

--- AnimacionGoku.py
+++ AnimacionGoku.py
@@ -9,7 +9,7 @@ direc=True
 i=0
 xixf={}
 Rxixf={}
-
+#IMAGEN#
 def imagen(filename, transparent=False):
     image=pygame.image.load(filename)
     image=image.convert()
@@ -18,3 +18,21 @@ def imagen(filename, transparent=False):
         color=image.get_at((0,0))
         image.set_colorkey(color,RLEACCEL)
     return image
+#TECLADO#
+def teclado():
+    teclado=pygame.key.get_pressed()
+    global MposX
+    global cont, direc
+
+    if teclado[K_RIGHT]:
+        MposX+=2
+        cont+=1
+        direc=True
+    elif teclado[K_LEFT]:
+        MposX-=2
+        cont+=1
+        direc=True
+    return
+
+#SPRITE#
+d
