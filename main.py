787f3c87b57d0658a3d0778d64eac3750ad80a6f
Fabricio\Jhon <maria.cordova01@epn.edu.ec>
2017-07-22 20:27:35 -0500

    main

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+import juego
+               
+pygame.init()
+ 
+pantalla = pygame.display.set_mode((600, 300))
+pygame.display.set_caption("DRAGON BALL Z")
+clock = pygame.time.Clock()
+player = juego.Serge((150, 150))
+fondo = pygame.image.load("fondo4.jpg")
+
+game_over = False
+ 
+while game_over == False:
+ 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+ 
+    player.handle_event(event)                 
+    pantalla.blit(fondo, (0,0)) 
+    pantalla.blit(player.image, player.rect) 
+    pygame.display.flip()              
+    clock.tick(10)
+ 
+pygame.quit ()
+
