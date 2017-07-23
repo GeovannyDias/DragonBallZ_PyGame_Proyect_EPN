beb68c1947662430018813825a3b65ec22077906
Fabricio\Jhon <maria.cordova01@epn.edu.ec>
2017-07-22 20:30:34 -0500

    funciones y metodos

--- juego1.py
+++ juego1.py
@@ -18,3 +18,19 @@ class Serge(pygame.sprite.Sprite):
         self.right_states = { 0: (530,0,75,95), 1: (605,0,60,90), 2: (670,0,75,90) }
         self.up_states = { 0: (120,65,45,90), 1: (165,60,55,90), 2: (220,65,45,90) }
         self.down_states = { 0: (265,100,65,90), 1: (330,90,47,90), 2: (377,90,70,90) }
+#Para animar a nuestro personaje, necesitamos repetidamente recorrer tres cuadros
+    def get_frame(self, frame_set):
+        self.frame += 1
+        if self.frame > (len(frame_set) - 1):
+            self.frame = 0
+        return frame_set[self.frame]
+#Creamos un método para recortar el área de cada trama.
+#Primero tenemos que comprobar si estamos tratando con múltiples marcos (movimiento) o un solo marco (de pie).
+#Si el personaje se está moviendo, los marcos son manejados por el método get_frame (). Si el personaje está simplemente de pie,
+#el marco es manejado directamente por las funciones incorporadas de Pygame.
+    def clip(self, clipped_rect):
+        if type(clipped_rect) is dict:
+            self.sheet.set_clip(pygame.Rect(self.get_frame(clipped_rect)))
+        else:
+            self.sheet.set_clip(pygame.Rect(clipped_rect))
+        return clipped_rect
